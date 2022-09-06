7c263ec34e1738416aa069f9b9a83e5265686286
gabrysera <gabrieleserafini10100@gmail.com>
2022-09-06 15:10:21 +0200

    created graph data structure

---
+++ src/data_structures/algorithms/ford_fulkerson.py
@@ -0,0 +1,6 @@
+from data_structures.graph import *
+
+#take a graph, the source, the sink, and then return the graph with the values of flow such that we have a maximum
+# flow and the min cut of this new graph 
+def ford_fulkerson(G:Graph, s:Node, t:Node) -> tuple[Graph, Bipartition]:
+    return 
---
+++ src/data_structures/graph.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, id):
+        self.id = id   
+        self.ingoing_edges = []
+        self.outgoing_edges = [] 
+
+    #maybe for these 2 functions make such that current node is always first in the edges in the list
+    def add_ingoing_edge(self, edge):
+        self.ingoing_edges.append(edge)
+
+    def add_outgoing_edge(self, edge):
+        self.outgoing_edges.append(edge)
+
+class Edge:
+    def __init__(self, x:Node, y:Node, flow:int = 0, capacity:int = 0):
+        if capacity < flow:
+            raise ValueError(f"flow cannot be bigger than capacity for edge {x,y}")
+        self.edge = (x,y)
+        x.add_outgoing_edge(self.edge)
+        y.add_ingoing_edge(self.edge)
+        
+class Graph:
+    
+    def __init__(self,V:list[Node],  E:list[Edge]):
+        def illegal_edges() -> bool:
+            for e in E:
+                if e[0] not in V or e[1] not in V:
+                    return False
+            return True
+
+        if illegal_edges():
+            raise ValueError(f"some of these edges contains vertex that are not in the graph!!")
+        self.E = E
+        self.V = V
+
+class Bipartition:
+
+    def __init__(self, G:Graph, S:list[Node], T:list[Node]):
+        self.G = G
+        self.S = S
+        self.T = T
